61365a70c205e6322b2734d4ad2e1f7d65329147
Kevin Winter <traashermc@gmail.com>
2023-06-25 17:13:09 +0200

    Updated XEEM to 1.20.1

--- src/main/java/net/xenyria/eem/mixin/DisconnectMixin.java
+++ src/main/java/net/xenyria/eem/mixin/DisconnectMixin.java
@@ -13,6 +13,7 @@ public class DisconnectMixin {
 
     @Inject(at = @At("HEAD"), method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V")
     public void handleDisconnect(CallbackInfo info) {
+
         PlayingSessionInformation.setOnNetwork(false);
         PlayingSessionInformation.setCurrentServerType(EXenyriaServerType.UNKNOWN);
         PlayingSessionInformation.setServerInstanceId("");
